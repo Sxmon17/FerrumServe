06420c82bfc70738cfc3d5a90880f8c5419b30fd
Simon Guglberger <65186979+Sxmon17@users.noreply.github.com>
2023-02-27 18:26:48 +0100

    🚀 Added: Client can now receive messages

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,4 +1,4 @@
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
 use std::error::Error;
@@ -8,8 +8,9 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
     let mut stream = TcpStream::connect("127.0.0.1:7878").await?;
     println!("created stream");
 
-    let result = stream.write_all(b"hello world\n").await;
-    println!("wrote to stream; success={:?}", result.is_ok());
-
-    Ok(())
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+        stream.write_all(input.as_bytes()).await?;
+    }
 }
